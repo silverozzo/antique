21e5a9a29c3c04826ba63be355ef89cb5108d917
silverozzo <silverozzo@gmail.com>
2023-07-03 19:33:22 +0700

    Добавляет заполнение резерва

--- board/board.go
+++ board/board.go
@@ -12,6 +12,7 @@ type Board struct {
 	clueStack      *deck.ClueStack
 	monsterStack   *deck.MonsterStack
 	expeditionDeck *deck.ExpeditionDeck
+	assetDeck      *deck.AssetDeck
 	doomTrack      int
 	omen           *omen.Omen
 	reserve        [4]*deck.AssetCard
@@ -24,26 +25,32 @@ func New(doomTrack int) *Board {
 		clueStack:      deck.NewClueStack(),
 		monsterStack:   deck.NewMonsterStack(),
 		expeditionDeck: deck.NewExpeditionDeck(),
+		assetDeck:      deck.NewAssetDeck(),
 		doomTrack:      doomTrack,
 		omen:           &omen.Omen{},
 		locations:      location.NewGraph(),
 	}
 
-	// делаем пока примитивно-упрощенно
 	board.OpenTopGate()
+	board.FillReserves()
 
 	return &board
 }
 
 // Открываем ворота с верху стопки врат
 func (board *Board) OpenTopGate() {
-	// Берем верхнюю карту со стопки врат
 	gate := board.gateStack.DiscardTop().(*deck.GateToken)
-	monster := board.monsterStack.DiscardTop().(*deck.MonsterToken)
+	monster := board.monsterStack.DiscardRandom().(*deck.MonsterToken)
 
 	location := board.locations.GetLocationByName(gate.GetLocationName())
 	location.SetGate(gate)
 	location.AddMonster(monster)
+}
 
-	// panic("not implemented yet")
+func (board *Board) FillReserves() {
+	for i, item := range board.reserve {
+		if item == nil {
+			board.reserve[i] = board.assetDeck.DiscardTop().(*deck.AssetCard)
+		}
+	}
 }
